39efec597e8c481028c9246bf730ca27309a15f7
Lars de Ridder <lars.de.ridder@gmail.com>
2016-05-17 10:19:01 +0200

    Latest changes

--- huzzah-demo.py
+++ huzzah-demo.py
@@ -13,7 +13,8 @@ gc.collect()
 gc.mem_free()
 
 gc.disable()
-list(range(1000))
+a = list(range(1000))
+b = list(range(1000))
 
 import time
 time.localtime()
@@ -32,8 +33,14 @@ machine.freq(16000000)
 machine.reset()
 
 import network
-wlan = network.WLAN(network.STA_IF)
-wlan.isconnected()
+wlan = network.WLAN(network.STA_IF) # create station interface
+wlan.active(True)       # activate the interface
+wlan.scan()             # scan for access points
+wlan.isconnected()      # check if the station is connected to an AP
+wlan.connect('essid', 'password') # connect to an AP
+wlan.config('mac')      # get the interface's MAC adddress
+wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
+
 ap = network.WLAN(network.AP_IF)
 ap.config(essid="MicroPython says hi to PyGrunn")
 ap.config(password="pygrunn")
